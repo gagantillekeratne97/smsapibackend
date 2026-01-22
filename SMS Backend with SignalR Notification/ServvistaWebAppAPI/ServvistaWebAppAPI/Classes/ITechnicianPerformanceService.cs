7829b2442212ddb8f8ffeea8b5244cc1e50701e1
Gagan Tillekeratne <110877796+gagantillekeratne97@users.noreply.github.com>
2026-01-22 14:18:57 +0530

    Delete SMS Backend with SignalR Notification directory

--- SMS Backend with SignalR Notification/ServvistaWebAppAPI/ServvistaWebAppAPI/Classes/ITechnicianPerformanceService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ServvistaWebAppAPI.Models;
-
-namespace ServvistaWebAppAPI.Classes
-{
-    public interface ITechnicianPerformanceService
-    {
-        Task<TechnicianPerformanceModel> GetPerformanceAsync(string techCode);
-    }
-}
--- SMS Backend with SignalR Notification/ServvistaWebAppAPI/ServvistaWebAppAPI/Models/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ServvistaWebAppAPI.Models
-{
-    public class LoginModel
-    {
-        public string TECH_CODE { get; set; }
-        public string Password { get; set; } 
-    }
-}
